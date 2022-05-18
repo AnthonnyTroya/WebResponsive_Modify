cf679932879aeebdedeb6e8a28da2a2551befe45
THONNY <atroyaarev@educacion.navarra.es>
2022-05-18 18:38:17 +0200

    Contact

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
-    navLinks.style.right = "0";
+  navLinks.style.right = "0";
 }
 
 function hideMenu() {
-    navLinks.style.right = "-200px";
+  navLinks.style.right = "-200px";
 }
 // navbar togglemenu end
 
@@ -15,49 +15,47 @@ function hideMenu() {
 const filterItem = document.querySelector(".items");
 const filterImg = document.querySelectorAll(".gallery .image");
 
-window.onload = ()=>{
-  filterItem.onclick = (selectedItem)=>{ 
-    if(selectedItem.target.classList.contains("item")){
-      filterItem.querySelector(".active").classList.remove("active"); 
-      selectedItem.target.classList.add("active"); 
+window.onload = () => {
+  filterItem.onclick = (selectedItem) => {
+    if (selectedItem.target.classList.contains("item")) {
+      filterItem.querySelector(".active").classList.remove("active");
+      selectedItem.target.classList.add("active");
       let filterName = selectedItem.target.getAttribute("data-name");
       filterImg.forEach((image) => {
-        let filterImges = image.getAttribute("data-name"); 
-     
-        if((filterImges == filterName) || (filterName == "all")){
-          image.classList.remove("hide"); 
+        let filterImges = image.getAttribute("data-name");
+
+        if (filterImges == filterName || filterName == "all") {
+          image.classList.remove("hide");
           image.classList.add("show");
-        }else{
-          image.classList.add("hide"); 
+        } else {
+          image.classList.add("hide");
           image.classList.remove("show");
         }
       });
     }
-  }
+  };
   for (let i = 0; i < filterImg.length; i++) {
-    filterImg[i].setAttribute("onclick", "preview(this)"); 
+    filterImg[i].setAttribute("onclick", "preview(this)");
   }
-}
-
+};
 
 const previewBox = document.querySelector(".preview-box"),
-categoryName = previewBox.querySelector(".title p"),
-previewImg = previewBox.querySelector("img"),
-closeIcon = previewBox.querySelector(".icon"),
-shadow = document.querySelector(".shadow");
+  categoryName = previewBox.querySelector(".title p"),
+  previewImg = previewBox.querySelector("img"),
+  closeIcon = previewBox.querySelector(".icon"),
+  shadow = document.querySelector(".shadow");
 
-function preview(element){
- 
+function preview(element) {
   document.querySelector("body").style.overflow = "hidden";
   let selectedPrevImg = element.querySelector("img").src;
-  let selectedImgCategory = element.getAttribute("data-name"); 
+  let selectedImgCategory = element.getAttribute("data-name");
   previewImg.src = selectedPrevImg;
-  categoryName.textContent = selectedImgCategory; 
-  previewBox.classList.add("show"); 
+  categoryName.textContent = selectedImgCategory;
+  previewBox.classList.add("show");
   shadow.classList.add("show");
-  closeIcon.onclick = ()=>{ 
-    previewBox.classList.remove("show"); 
-    shadow.classList.remove("show"); 
-    document.querySelector("body").style.overflow = "auto"; 
-  }
+  closeIcon.onclick = () => {
+    previewBox.classList.remove("show");
+    shadow.classList.remove("show");
+    document.querySelector("body").style.overflow = "auto";
+  };
 }
